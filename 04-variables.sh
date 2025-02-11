1301ff320346056fe3b3844d0cba28cd7ffdfb60
ggopichandu <gummagopichand@gmail.com>
2025-02-11 23:54:20 +0530

    first commit

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# declaring variables
-PERSON1=Gopichand # no spaces b/w = and value
-PERSON2=Siri
+PERSON1=$1 # no spaces b/w = and value
+PERSON2=$2
 
-# refering variables
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine too. How is your work?"
