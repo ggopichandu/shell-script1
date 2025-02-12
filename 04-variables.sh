54378b3ffabce686acbe0fcdd39bcb866a5bbddf
ggopichandu <gummagopichand@gmail.com>
2025-02-12 22:47:41 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,6 +4,6 @@ PERSON1=$1 # no spaces b/w = and value
 PERSON2=$2
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine too. How is your work?"
 echo "$PERSON2:: Not bad. I am thinking to upgrade to DevOps"
---
+++ 06-array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MOVIES=("Dil" "Mirchi" "RRR")
+
+# size of above array is 3.
+# Index are 0,1,2
+
+# list is always starts with 0.
+
+echo "First Movie is: ${MOVIE[0]}"
+echo "Second Movie is: ${MOVIE[1]}"
+echo "Third Movie is: ${MOVIE[2]}"
+
