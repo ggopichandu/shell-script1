105039e10b0741333dc11e035ec38b46a6bdfe11
ggopichandu <gummagopichand@gmail.com>
2025-02-13 12:57:30 +0530

    installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access."
+    exit 1 # manually exit if any error comes
 else
     echo "You are super user."
 fi        
